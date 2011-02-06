4ee2ef5b929f62a4ae66f655f8df5c93745fbf76
Marc Hesse <marc.hesse@gmail.com>
2011-02-06 01:06:36 +0100

    fixed exception when clearing an unexpired timer

--- libCommon/src/LinuxTimer.cpp
+++ libCommon/src/LinuxTimer.cpp
@@ -4,7 +4,7 @@
 #include "Exception.h"
 #include <sys/timerfd.h>
 #include <fcntl.h>
-#include <cstring>
+#include <errno.h>
 
 LinuxTimer::LinuxTimer() :
   WaitObject(timerfd_create(CLOCK_MONOTONIC, O_NONBLOCK))
@@ -23,7 +23,7 @@ void LinuxTimer::start(uint32_t timeout)
   itimerspec elapseTime;
 
   elapseTime.it_interval.tv_sec = timeout / 1000;
-  elapseTime.it_interval.tv_nsec = timeout * 1000000;
+  elapseTime.it_interval.tv_nsec = (timeout % 1000) * 1000000;
   elapseTime.it_value.tv_sec = 0;
   elapseTime.it_value.tv_nsec = 0;
 
@@ -34,7 +34,11 @@ void LinuxTimer::start(uint32_t timeout)
 void LinuxTimer::stop()
 {
   itimerspec elapseTime;
-  memset(&elapseTime, 0, sizeof(elapseTime));
+
+  elapseTime.it_interval.tv_sec = 0;
+  elapseTime.it_interval.tv_nsec = 0;
+  elapseTime.it_value.tv_sec = 0;
+  elapseTime.it_value.tv_nsec = 0;
 
   if(timerfd_settime(getHandle(), 0, &elapseTime, NULL) != 0)
     throw Exception("Failed to stop timer: " + lastError());
@@ -45,6 +49,6 @@ void LinuxTimer::clear()
   stop();
 
   uint64_t buffer;
-  if(read(getHandle(), &buffer, sizeof(buffer)) != sizeof(buffer))
+  if(read(getHandle(), &buffer, sizeof(buffer)) != sizeof(buffer) && errno != EAGAIN)
     throw Exception("Failed to clear timer: " + lastError());
 }

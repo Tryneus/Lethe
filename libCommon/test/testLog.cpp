631995c521bd74f6f7977e47463b2fe928b1517d
Marc Hesse <marc.hesse@gmail.com>
2011-02-11 20:36:02 +0100

    removed extraneous test stubs

--- libCommon/test/testLog.cpp
+++ libCommon/test/testLog.cpp
@@ -3,31 +3,6 @@
 #include "Log.h"
 #include "catch.hpp"
 
-TEST_CASE("log/setLevel", "Test changing the logging severity level")
-{
-  // TODO: implement log/setLevel
-}
-
-TEST_CASE("log/logError", "Test logging at the error level")
-{
-  // TODO: implement log/logError
-}
-
-TEST_CASE("log/logInfo", "Test logging at the info level")
-{
-  // TODO: implement log/logInfo
-}
-
-TEST_CASE("log/logDebug", "Test logging at the debug level")
-{
-  // TODO: implement log/logDebug
-}
-
-TEST_CASE("log/stdOutMode", "Test logging to stdout")
-{
-  // TODO: implement log/stdOutMode
-}
-
 TEST_CASE("log/fileMode", "Test logging to a file")
 {
   // TODO: implement log/fileMode

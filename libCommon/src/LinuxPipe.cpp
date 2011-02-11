9fca6f6e5976e0dec04317275c954207dd81efca
Marc Hesse <marc.hesse@gmail.com>
2011-02-11 06:00:26 +0100

    added asynchronous i/o to pipe

--- libCommon/src/LinuxPipe.cpp
+++ libCommon/src/LinuxPipe.cpp
@@ -2,18 +2,21 @@
 #include "AbstractionTypes.h"
 #include "AbstractionFunctions.h"
 #include "Exception.h"
+#include <queue>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+#include <aio.h>
+#include "Log.h"
 
 LinuxPipe::LinuxPipe() :
   WaitObject(INVALID_HANDLE_VALUE),
   m_pipeRead(INVALID_HANDLE_VALUE),
   m_pipeWrite(INVALID_HANDLE_VALUE),
-  m_pendingData(NULL),
-  m_pendingSend(NULL),
-  m_pendingSize(0)
+  m_asyncStart(0),
+  m_asyncEnd(0),
+  m_blockingWrite(false)
 {
   int pipes[2];
 
@@ -34,45 +37,96 @@ LinuxPipe::LinuxPipe() :
   }
 
   setWaitHandle(m_pipeRead);
+
+  // Prepare async event structures
+  memset(m_asyncArray, 0, sizeof(m_asyncArray));
+  for(uint32_t i = 0; i < s_maxAsyncEvents; ++i)
+  {
+    m_asyncArray[i].aio_fildes = m_pipeWrite;
+  }
 }
 
 LinuxPipe::~LinuxPipe()
 {
-  delete [] m_pendingData;
+  std::queue<aiocb*> unfinishedEvents;
+
+  // Destroy any asynchronous events and delete buffers
+  for(uint32_t i = m_asyncStart; i != m_asyncEnd; i = (i + 1) % s_maxAsyncEvents)
+  {
+    if(aio_cancel(m_pipeWrite, &m_asyncArray[i]) != AIO_CANCELED)
+      unfinishedEvents.push(&m_asyncArray[i]);
+    else
+      delete [] reinterpret_cast<volatile uint8_t*>(m_asyncArray[i].aio_buf);
+  }
 
   close(m_pipeWrite);
   close(m_pipeRead);
+
+  while(!unfinishedEvents.empty())
+  {
+    int status = EINPROGRESS;
+
+    // TODO: polling sucks, maybe rewrite to use kernel-based aio with eventfd rather than libc-based aio
+    while(status == EINPROGRESS || status == EAGAIN)
+    {
+      Sleep(5);
+      status = aio_error(unfinishedEvents.front());
+    }
+
+    // This results in a memory leak in aio =(
+    delete [] reinterpret_cast<volatile uint8_t*>(unfinishedEvents.front()->aio_buf);
+    aio_return(unfinishedEvents.front());
+    unfinishedEvents.pop();
+  }
 }
 
-void LinuxPipe::send(uint8_t* buffer, uint32_t bufferSize)
+void LinuxPipe::getAsyncResults()
 {
-  ssize_t bytesWritten;
-
-  // If we failed a send before, try to send the remainder now
-  if(m_pendingData != NULL)
+  for(uint32_t i = m_asyncStart; i != m_asyncEnd; i = (i + 1) % s_maxAsyncEvents)
   {
-    bytesWritten = write(m_pipeWrite, m_pendingSend, m_pendingSize);
+    int asyncResult = aio_error(&m_asyncArray[i]);
 
-    if(bytesWritten != m_pendingSize)
-    {
-      if(bytesWritten > 0)
-      {
-        m_pendingSend += bytesWritten;
-        m_pendingSize -= bytesWritten;
-      }
+    if(asyncResult == EINPROGRESS || asyncResult == EAGAIN)
+      break;
 
-      throw OutOfMemoryException("LinuxPipe");
-    }
-    else
-    {
-      delete [] m_pendingData;
-      m_pendingData = NULL;
-      m_pendingSend = NULL;
-      m_pendingSize = 0;
-    }
+    aio_return(&m_asyncArray[i]);
+    delete [] reinterpret_cast<volatile uint8_t*>(m_asyncArray[i].aio_buf);
+    m_asyncStart = (m_asyncStart + 1) % s_maxAsyncEvents;
+  }
+}
+
+void LinuxPipe::asyncWrite(const void* buffer, uint32_t bufferSize)
+{
+  if(m_asyncStart == (m_asyncEnd + 1) % s_maxAsyncEvents)
+    throw OutOfMemoryException("LinuxPipe::write");
+
+  struct aiocb* asyncEvent = &m_asyncArray[m_asyncEnd];
+
+  asyncEvent->aio_buf = new uint8_t[bufferSize];
+  asyncEvent->aio_nbytes = bufferSize;
+
+  memcpy(const_cast<void*>(asyncEvent->aio_buf), buffer, bufferSize);
+
+  if(aio_write(asyncEvent) != 0)
+    throw Exception("Failed to create async event: " + lastError());
+
+  m_asyncEnd = (m_asyncEnd + 1) % s_maxAsyncEvents;
+}
+
+void LinuxPipe::send(const void* buffer, uint32_t bufferSize)
+{
+  getAsyncResults();
+
+  if(m_asyncStart != m_asyncEnd)
+    return asyncWrite(buffer, bufferSize);
+
+  if(m_blockingWrite)
+  {
+    fcntl(m_pipeWrite, F_SETFL, O_NONBLOCK);
+    m_blockingWrite = false;
   }
 
-  bytesWritten = write(m_pipeWrite, buffer, bufferSize);
+  int bytesWritten = write(m_pipeWrite, buffer, bufferSize);
 
   if(bytesWritten < 0)
   {
@@ -82,19 +136,16 @@ void LinuxPipe::send(uint8_t* buffer, uint32_t bufferSize)
       throw Exception("Failed to write to pipe: " + lastError());
   }
 
-  // TODO: this leaves the possibility of an incomplete message if there is not active traffic
-  // This should only happen on chunks of data larger than the pipe buffer, though
-  if(bytesWritten < static_cast<ssize_t>(bufferSize))
+  if(static_cast<uint32_t>(bytesWritten) < bufferSize)
   {
-    // The write was not complete, store the remainder to send later
-    m_pendingData = new uint8_t[bufferSize - bytesWritten];
-    m_pendingSend = m_pendingData;
-    m_pendingSize = bufferSize - bytesWritten;
-    memcpy(m_pendingSend, buffer + bytesWritten, m_pendingSize);
+    m_blockingWrite = true;
+    fcntl(m_pipeWrite, F_SETFL, 0);
+    asyncWrite(((uint8_t*)buffer) + bytesWritten, bufferSize - bytesWritten);
   }
+
 }
 
-uint32_t LinuxPipe::receive(uint8_t* buffer, uint32_t bufferSize)
+uint32_t LinuxPipe::receive(void* buffer, uint32_t bufferSize)
 {
   ssize_t bytesRead(read(m_pipeRead, buffer, bufferSize));
 

9cc7146fc52195d172ec547149234afde2fc8bc0
Marc Hesse <marc.hesse@gmail.com>
2011-02-27 12:26:57 -0700

    added test stubs for inter-process streams

---
+++ libProcessComm/test/testByteStream.cpp
@@ -0,0 +1,6 @@
+#include "catch.hpp"
+
+TEST_CASE("byteStream/structor", "Test construction and destruction of ProcessByteStream")
+{
+  // TODO: implement byteStream/structor test
+}
---
+++ libProcessComm/test/testMessageStream.cpp
@@ -0,0 +1,52 @@
+#include "Lethe.h"
+#include "ProcessComm.h"
+#include "ThreadComm.h"
+#include "catch.hpp"
+
+using namespace lethe;
+
+class MessageStreamThread : public Thread
+{
+private:
+  ByteStream& m_byteStream;
+  MessageStream* m_messageStream;
+
+public:
+  MessageStreamThread(ByteStream& byteStream) :
+    Thread(INFINITE),
+    m_byteStream(byteStream),
+    m_messageStream(NULL)
+  {
+    // Do nothing
+  }
+
+  ~MessageStreamThread()
+  {
+    delete m_messageStream;
+  }
+
+  void setup()
+  {
+    m_messageStream = new ProcessMessageStream(m_byteStream, 2000, 4000);
+  }
+
+  void iterate(Handle handle GCC_UNUSED)
+  {
+    // Do nothing
+  }
+};
+
+TEST_CASE("messageStream/structor", "Test construction and destruction of a ProcessMessageStream")
+{
+  // Use ThreadByteStreams since we're in the same process
+  for(uint32_t i = 0; i < 1000; ++i)
+  {
+    ThreadByteConnection byteConnection;
+    ByteStream& byteStream = byteConnection.getStreamA();
+    MessageStreamThread thread(byteConnection.getStreamB());
+
+    thread.start();
+
+    ProcessMessageStream messageStream(byteStream, 2000, 4000);
+  }
+}

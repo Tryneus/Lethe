5974c23235f261104cbf3ecbf5069acf2e12e4b9
Marc Hesse <marc.hesse@gmail.com>
2011-05-04 16:34:11 +0200

    fixed some linux compilation problems

--- LetheCommon/src/linux/LinuxTimer.cpp
+++ LetheCommon/src/linux/LinuxTimer.cpp
@@ -3,6 +3,9 @@
 #include "LetheFunctions.h"
 #include "LetheException.h"
 #include <sys/timerfd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
 

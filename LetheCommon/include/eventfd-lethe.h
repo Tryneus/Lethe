a3fa1487c527effe408a03ae3da079f1c8ebf20a
Marc Hesse <marc.hesse@gmail.com>
2011-09-05 20:12:13 +0200

    added ioctl command to check the mode of an eventfd

--- LetheCommon/include/eventfd-lethe.h
+++ LetheCommon/include/eventfd-lethe.h
@@ -3,6 +3,14 @@
 
 #include <sys/ioctl.h>
 
+// Modes
+enum eventfd_mode_t
+{
+  EFD_SEMAPHORE_MODE,
+  EFD_MUTEX_MODE,
+  EFD_EVENT_MODE
+};
+
 // IOCTL operations that may be performed on the lethe-eventfd device
 #define EVENTFD_LETHE_MAJOR 245
 
@@ -12,5 +20,6 @@
 #define EFD_SET_EVENT_MODE _IOW(EVENTFD_LETHE_MAJOR, 3, unsigned long)
 #define EFD_SET_MAX_VALUE _IOW(EVENTFD_LETHE_MAJOR, 4, unsigned long)
 #define EFD_SET_ERROR _IOW(EVENTFD_LETHE_MAJOR, 5, bool)
+#define EFD_GET_MODE _IO(EVENTFD_LETHE_MAJOR, 6)
 
 #endif
